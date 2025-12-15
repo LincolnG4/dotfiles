db21ce6162faf2fafe5022bfa4e555b114eb9308
SAG4CT <sag4ct@bosch.com>
2025-12-15 13:47:03 +0100

    fix golsp

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -2,5 +2,14 @@ return {
   "neovim/nvim-lspconfig",
   opts = {
     inlay_hints = { enabled = false },
+    servers = {
+      gopls = {
+        analyses = {
+          unusedparams = true,
+        },
+        staticcheck = true,
+        gofumpt = true,
+      },
+    },
   },
 }
